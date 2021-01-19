910f9eb450d8ab7ca3529c495ad88388e78f3529
rohankumarthakran@gmail.com <dec@gmail.com>
2021-01-19 14:37:09 +0530

    Api fetch done

--- src/Row.js
+++ src/Row.js
@@ -1,22 +1,33 @@
-import React, {useSate,useEffect} from 'react'
+import React, {useState,useEffect} from 'react'
 import axios from './axious';
+import "./Row.css";
+const baseUrl ="https://image.tmdb.org/t/p/w200";
 function Row({title,fetchURL}) {
-
+    const [movies,setMovies] = useState([]);
  
     useEffect(() =>{
         async function fetchData () {
             const request = await axios.get(fetchURL)
-            console.log(request);
+            setMovies(request.data.results);
             return request; 
         }
         fetchData();
-    },[])
+    },[fetchURL]); //here we are using fetchURL because we are using it from outside
 
+    console.table(movies);
     return (
        <React.Fragment>
-        {/* Title*/}
-        <h1>This is a row {title}</h1>
-        {/*container -> postion */}
+        <div className ="row">
+            <h1>{title}</h1>
+        </div>
+
+        <div className ="row_posters">
+            {movies.map((item,index) =>{
+               return( 
+                <img className ="row_poster"src={`${baseUrl}${item.poster_path}`} alt={item.name}/>
+               )
+            })}
+        </div>
        </React.Fragment>
     )
 }
